749e9177a8352221f1dd0be00a14a050b0e2b0de
omriKaltrua <68190592+omriKaltura@users.noreply.github.com>
2021-05-06 10:02:38 +0300

    Update after_deploy.sh
    new jenkins conf

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -k -d "{'name':$1, 'version':$2, 'source':'npm'}" -H "Content-Type: application/json" -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$3
+#curl -k -d "{'name':$1, 'version':$2, 'source':'npm'}" -H "Content-Type: application/json" -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$3
+curl -k -d "{'version':$2}" -H "Content-Type: application/json" -X POST https://jenkins-central.prod.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$3
