7ba889aabecb9078ba748147be04f4a61d6f8f5f
yuval.keidar <keidaryu@gmail.com>
2021-06-02 11:37:40 +0300

    Update after_deploy.sh

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -13,10 +13,9 @@ packageName="playkit-${repoName#$REPO_PREFIX}"
 echo "$packageName"
 echo "$currentVersion"
 
-REAL_BRANCH=$(git ls-remote origin | sed -n "\|$TRAVIS_COMMIT\s\+refs/heads/|{s///p}")
-echo "$REAL_BRANCH"
+TAGGED_BRANCH=$(git ls-remote origin | sed -n "\|$TRAVIS_COMMIT\s\+refs/heads/|{s///p}")
 UPDATE_UI_CONF=true
-if [ "$REAL_BRANCH" = "master" ]; then
+if [ "$TAGGED_BRANCH" = "master" ]; then
   UPDATE_UI_CONF=true
 else
   UPDATE_UI_CONF=false
